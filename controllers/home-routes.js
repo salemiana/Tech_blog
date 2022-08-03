e00679bd0a4ca5fa3f156d5711ed5767e2de5a3e
salem z <salem.zerezghi2@gmail.com>
2022-08-02 19:51:24 -0600

    signup functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,18 +1,17 @@
-const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
-const router = require('express').Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
-    Post.findAll(
-        {
-            attributes: ["id", "title", "content", "created_at"],
+router.get("/", (req, res) => {
+  Post.findAll({
+    attributes: ["id", "title", "content", "created_at"],
     include: [
       {
         model: Comment,
         attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
         include: {
           model: User,
-          attributes: ['username'],
+          attributes: ["username"],
         },
       },
       {
@@ -30,5 +29,13 @@ router.get('/', (req, res) => {
       res.status(500).json(err);
     });
 });
-       
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
 module.exports = router;
